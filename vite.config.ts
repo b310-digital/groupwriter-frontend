973703d2bacddb31b85de00d80142878444ddcd3
Jannik Streek <jannik.streek@gmail.com>
2025-02-03 15:18:38 +0100

    fix pdf download and stylings of lists

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // The html2canvas library does not support oklch() color function from tailwind
+  // Therefore, we overwirte and use html2canvas-pro instead
+  resolve: {
+    alias: {
+      html2canvas: path.resolve(__dirname, 'node_modules/html2canvas-pro')
+    }
+  },
   plugins: [react()],
   build: {
     assetsInlineLimit: 0
