5f520a7ec0c47a70c968e0e0e7aca40e5c334aac
Jannik Streek <jannik.streek@gmail.com>
2025-03-07 15:28:11 +0100

    fix tests locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
-const connectOptions = {
+const connectOptions = process.env.TESTING_PLAYWRIGHT_WS_ENDPOINT ? {
   connectOptions: {
     wsEndpoint: process.env.TESTING_PLAYWRIGHT_WS_ENDPOINT ?? '',
   },
-}
+} : {}
 
-export default defineConfig({...{
-  testDir: './tests',
+export default defineConfig({
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -19,13 +19,13 @@ export default defineConfig({...{
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  use: { ...{
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.TESTING_PLAYWRIGHT_BASE_URL ?? 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
+  }, ...connectOptions },
 
   /* Configure projects for major browsers */
   projects: [
@@ -40,6 +40,4 @@ export default defineConfig({...{
     command: 'npm run dev',
     reuseExistingServer: !process.env.CI,
   },
-}, ...(process.env.TESTING_PLAYWRIGHT_WS_ENDPOINT ? connectOptions : {})});
-
-
+});
