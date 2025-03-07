ef073e3588238f85e00e79a0426fc19ea365ba57
Jannik Streek <jannik.streek@gmail.com>
2025-03-07 15:37:43 +0100

    set url

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: { ...{
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.TESTING_PLAYWRIGHT_BASE_URL ?? 'http://127.0.0.1:5173',
+    baseURL: process.env.TESTING_PLAYWRIGHT_BASE_URL ?? 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -37,6 +37,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
+    url: 'http://localhost:5173',
     command: 'npm run dev',
     reuseExistingServer: !process.env.CI,
   },
