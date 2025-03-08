6d1823ba4fdbfe22e7fc9786ce067014ce7175f8
Jannik Streek <jannik.streek@gmail.com>
2025-03-08 10:22:10 +0100

    add server var

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -25,6 +25,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   }, ...connectOptions },
 
   /* Configure projects for major browsers */
@@ -34,7 +35,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     }
   ],
-
+  outputDir: './playwright/output',
   /* Run your local dev server before starting the tests */
   webServer: {
     port: 5173,
