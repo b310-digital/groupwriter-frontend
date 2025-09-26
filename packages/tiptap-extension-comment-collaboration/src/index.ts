612c8e31d20c6c86e2ca6005cf8117d436579a52
Jannik Streek <jannik.streek@gmail.com>
2025-09-26 15:35:50 +0200

    add comment collaboration extension back in

---
+++ packages/tiptap-extension-comment-collaboration/src/index.ts
@@ -0,0 +1,4 @@
+import { CollaborationCommentsExtension, commentRemoveRegex } from './collaboration-comments.js'
+export { CommentOptions, CommentItem, CommentType, CommentStorage, MarkWithPos } from './types.js'
+
+export { CollaborationCommentsExtension as default, CollaborationCommentsExtension, commentRemoveRegex }
